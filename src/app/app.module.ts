0f76f00b311e6791b7bcaca376ddea1f10407cb7
Leandro Sorolla <leandrosorolla@gmail.com>
2020-02-13 20:40:53 -0300

    borrado de componente modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,6 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TablaComponent } from './componentes/tabla/tabla.component';
-//import { ModalComponent } from './componentes/modal/modal.component';
 import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 import { NavbarComponent } from './componentes/navbar/navbar.component';
